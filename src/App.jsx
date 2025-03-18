9f5bdbbe21b619673f5b5187c52ae9ecacaad224
Suyash7987 <104486757+Suyash7987@users.noreply.github.com>
2025-03-18 18:26:58 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,101 +1,22 @@
+import React, { useState } from "react";
+import Footer from "./components/Footer";
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Contact from "./components/Contact";
-import Home from "./components/Home";
-import MyNav from "./components/MyNav";
-import About from "./components/About";
-import SignUp from "./components/Signup";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Product from "./components/Product";
-import Loading from "./components/Loading";
-import Profile from "./components/Profile";
-import { useState } from "react";
+import MyNav from"./components/MyNav";
+import { Outlet } from "react-router-dom";
 
-function App() {
+
+const App = () => {
   const [userLogined, setUserLogined] = useState(
     localStorage.getItem("userLogined") === "true"
   );
-  const route = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <>
-          <MyNav userLogined={userLogined} />
-          <Home />
-        </>
-      ),
-    },
-    {
-      path: "/About",
-      element: (
-        <>
-          <MyNav userLogined={userLogined} />
-          <About />
-        </>
-      ),
-    },
-    {
-      path: "/Contact",
-      element: (
-        <>
-          <MyNav userLogined={userLogined} />
-          <Contact />
-        </>
-      ),
-    },
-    {
-      path: "/Product",
-      element: (
-        <>
-          <MyNav userLogined={userLogined} />
-          <Product />
-        </>
-      ),
-      children:[
-        {
-          path:"/View",
-          element:<View/>
-        }
-      ]
-    },
-    {
-      path: "/Login",
-      element: (
-        <>
-          <MyNav userLogined={userLogined} />
-          <Loading userLogined={userLogined} setUserLogined={setUserLogined} />
-        </>
-      ),
-    },
-    {
-      path: "/Signup",
-      element: (
-        <>
-          <MyNav userLogined={userLogined} />
-          <SignUp />
-        </>
-      ),
-    },
-    {
-      path: "/Profile",
-      element: (
-        <>
-          <MyNav userLogined={userLogined} />
-          <ProtectedRoute redirectTo="/Profile" userLogined={userLogined}>
-            <Profile
-              userLogined={userLogined}
-              setUserLogined={setUserLogined}
-            />
-          </ProtectedRoute>
-        </>
-      ),
-    },
-  ]);
   return (
-    <>
-      <RouterProvider router={route} />
-    </>
+  
+      <div>
+        <MyNav  userLogined={userLogined} setUserLogined={setUserLogined}/>
+        <Outlet />
+      </div>
+  
   );
-}
+};
 
 export default App;
